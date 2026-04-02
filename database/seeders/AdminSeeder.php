e6f71baa88c14271a27c079465b01464d0a755a5
SASwagoto <saswagoto@gmail.com>
2026-04-02 21:07:27 +0600

    Update Seeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class AdminSeeder extends Seeder
 {
@@ -17,8 +18,27 @@ class AdminSeeder extends Seeder
             'name' => 'Admin User',
             'username' => 'admin',
             'email' => 'admin@gmail.com',
-            'password' => bcrypt('12345678'), 
+            'password' => Hash::make('12345678'),
             'role' => 'admin'
+        ]);
+
+        User::create([
+            'name' => 'Test Driver',
+            'username' => 'driver01',
+            'email' => 'driver@swagoto.com',
+            'password' => Hash::make('12345678'),
+            'role' => 'driver',
+            'phone' => '01700000000',
+        ]);
+
+        // ৩. টেস্ট প্যাসেঞ্জার তৈরি করা
+        User::create([
+            'name' => 'Test Student',
+            'username' => 'student01',
+            'email' => 'student@swagoto.com',
+            'password' => Hash::make('12345678'),
+            'role' => 'passenger',
+            'student_id' => '2220000000',
         ]);
     }
 }
